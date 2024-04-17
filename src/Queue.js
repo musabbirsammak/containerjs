7114967eaa59677c23b936977761973c0b2ea040
musabbirsammak <musabbirhasansammak@gmail.com>
2024-04-17 20:53:02 +0600

    Adds documentations to Queue.js

--- src/Queue.js
+++ src/Queue.js
@@ -1,12 +1,23 @@
 class Queue {
+    /**
+     * Initializes a new queue with the given capacity.
+     * 
+     * @param {Number} Initial capacity. 
+     */
     constructor(capacity) {
         this.q = Array(capacity);
         this.capacity = this.capacity;
-        this.size = 0;
+        this.length = 0;
         this.head = -1;
         this.tail = -1;
     }
 
+    /**
+     * Enqueues the given item in the queue.
+     * 
+     * @param {Any} item Item to be enqueued.
+     * @throws {RangeError} Queue overflow.
+     */
     enqueue(item) {
         if (this.isFull) {
             throw new RangeError("Queue overflow.");
@@ -19,6 +30,12 @@ class Queue {
         this.size++;
     }
 
+    /**
+     * Returns the first element from the queue.
+     * 
+     * @returns {Any} First element.
+     * @throws {RangeError} Queue underflow.
+     */
     dequeue() {
         if (this.isEmpty()) {
             throw new RangeError("Queue underflow.");
@@ -34,6 +51,11 @@ class Queue {
         }
     }
 
+    /**
+     * Returns the first element of the queue.
+     *  
+     * @returns {Any} First element of the array. 
+     */
     peek() {
         if (this.isEmpty()) {
             throw new RangeError("Queue underflow.");
@@ -42,10 +64,20 @@ class Queue {
         }
     }
 
+    /**
+     * Whether the queue is full or not.
+     * 
+     * @returns {Boolean} True if the queue is full, false otherwise. 
+     */
     isFull() {
         return ((this.head == 0) && (this.tail == this.capacity - 1)) || ((this.tail + 1) == this.head);
     }
 
+    /**
+     * Whether the queue is empty or not.
+     * 
+     * @returns {Boolean} True if the queue is empty, false otherwise. 
+     */
     isEmpty() {
         return (this.head == -1) && (this.tail == -1);
     }
